8845f44a165ca81b87c131a5b9046fb8cd5767b4
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-13 17:11:06 +0600

    added user model

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
-require("./db/db");
-require("dotenv").config();
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const fileupload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 
-const port = process.env.PORT || 3000;
+const authRouter = require("./routes/authRoute");
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+//cloudinary configation
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
+
+//middleware
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
+app.use(express.json());
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1", authRouter);
+app.use("/api/v1", userRouter);
+app.use("/api/v1", restaurantRouter);
+app.use("/api/v1", orderRouter);
+
+app.get("/", (req, res) => {
+  // console.log(req)
+  res.send("Welcome to Food Delivery System");
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    msg: err.message,
+  });
+});
+
+module.exports = app;
---
+++ src/models/user.model.js
@@ -0,0 +1,127 @@
+const { Schema, model } = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 6 characters"],
+      trim: true,
+      //   validate: {
+      //     validator: function (password) {
+      //       const passwordRegex =
+      //         /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+      //       return passwordRegex.test(password);
+      //     },
+      //     message:
+      //       "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character",
+      //   },
+    },
+    address: {
+      city: {
+        type: String,
+        trim: true,
+        default: null,
+      },
+      street: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+    },
+    phone: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    image: {
+      id: String,
+      secure_url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordTokenExpiry: {
+      type: Date,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = async function () {
+  const token = jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.JWT_SECRET
+  );
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+
+  return token;
+};
+
+userSchema.methods.getforgotPasswordToken = async function () {
+  const token = crypto.randomBytes(32).toString("hex");
+
+  this.forgotPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  this.forgotPasswordTokenExpiry = Date.now() + 5 * 60 * 1000;
+
+  return token;
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
