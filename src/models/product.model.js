b1e9638dcd2a008c56730402c44100b7aefc83ef
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-14 20:12:57 +0600

    add product model

---
+++ src/models/product.model.js
@@ -0,0 +1,66 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema({
+  productName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  descriptiion: {
+    type: String,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      review: {
+        type: String,
+        trim: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+const ProductModel = model("Products", productSchema);
+
+module.exports = ProductModel;
